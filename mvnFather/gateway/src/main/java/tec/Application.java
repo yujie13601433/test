acdf75ccebbf9d8af3a1e7c9525a2eb2020bd304
“yujie13601433” <609793185@qq.com>
2022-06-22 10:28:03 +0800

    yyy

--- mvnFather/gateway/src/main/java/tec/Application.java
+++ mvnFather/gateway/src/main/java/tec/Application.java
@@ -6,6 +6,8 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @SpringBootApplication
 @EnableDiscoveryClient
 public class Application {
+
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class,args);
     }
